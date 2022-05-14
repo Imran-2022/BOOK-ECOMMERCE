956a5d69d67773caa544584d9695ca9de557c0eb
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-14 23:58:57 +0600

    auth form added

--- src/components/authentication/signUp/SignUp.js
+++ src/components/authentication/signUp/SignUp.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import "./SignUp.css"
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 const SignUp = () => {
     return (
-        <div>
-            <p>Sign up page</p>
-        </div>
+        <>
+            <div>
+                <div style={{ height: '60vh' }} className="w-75 h-91 m-auto row justify-content-center align-content-center">
+                    <Form className="w-50">
+                    <div className="mb-55">
+                    <Link  className='me-2' to="/home">Home</Link>/<Link className='ms-2'   to="/sign-up">SignUp</Link>
+                    </div>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button className="w-100" variant="primary" type="submit">
+                            Sign Up
+                        </Button>
+                        <div className="d-flex justify-content-between mt-1">
+                            <Link className="text-link" to="/sign-in">Already Sign-up ?</Link>
+                            <Link className="text-link" to="/sign-in">Sign In</Link>
+                        </div>
+                        <p className="text-center">OR</p>
+                        <p className="w-100 btn btn-outline-primary">Login with google</p>
+                    </Form>
+
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/authentication/singIn/SignIn.js
+++ src/components/authentication/singIn/SignIn.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import Navigation from '../../shared/navigation/Navigation';
 import "./SignIn.css"
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 const SignIn = () => {
     return (
-        <div>
-            <Navigation/>
-            <p>signin page</p>
-        </div>
+        <>
+            <div>
+                <div style={{ height: '60vh' }} className="w-75 h-91 m-auto row justify-content-center align-content-center">
+                    <Form className="w-50">
+                    <div className="mb-55">
+                    <Link  className='me-2' to="/home">Home</Link>/<Link className='ms-2'   to="/sign-in">SignIn</Link>
+                    </div>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button className="w-100" variant="primary" type="submit">
+                            SIGN IN
+                        </Button>
+                        <div className="d-flex justify-content-between mt-1">
+                            <Link className="text-link" to="/sign-in">Forget Password ?</Link>
+                            <Link className="text-link" to="/sign-up">Sign Up</Link>
+                        </div>
+                        <p className="text-center">OR</p>
+                        <p className="w-100 btn btn-outline-primary">Login with google</p>
+                    </Form>
+                </div>
+            </div>
+        </>
     );
 };
 
